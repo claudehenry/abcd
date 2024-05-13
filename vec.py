c93487b6c0e07c823a4853fb2bcd9b2ba0724287
claudehenry <chrochenoir@gmail.com>
2024-05-13 14:35:28 +0100

    make a change

--- vec.py
+++ vec.py
@@ -1,4 +1,7 @@
 
 class Vec:
-    def __init__(*vs):
+    def __init__(self, *vs):
         self.vs = vs
+
+    def __add__(self, other):
+        self.vs = [a * b for (a, b) in self.vs.zip(other.vs)]
